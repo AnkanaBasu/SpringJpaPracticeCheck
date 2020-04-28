df090cb311778d6e70e12ca4bb5a70e30b6e5e4b
AnkanaBasu <62287258+AnkanaBasu@users.noreply.github.com>
2020-04-28 18:02:38 +0530

    Add files via upload

---
+++ truyum-spring/src/main/java/com/cognizant/truyum/dao/MenuItemDaoCollectionImpl.java
@@ -0,0 +1,62 @@
+package com.cognizant.truyum.dao;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.cognizant.truyum.model.MenuItem;
+@Component
+public class MenuItemDaoCollectionImpl implements MenuItemDao {
+	@Autowired
+	private List<MenuItem> menuItemList;
+	@Override
+	public List<MenuItem> getMenuItemListAdmin() {
+		// TODO Auto-generated method stub
+		return menuItemList;
+	}
+
+	@Override
+	public List<MenuItem> getMenuItemListCustomer() {
+		// TODO Auto-generated method stub
+		List<MenuItem> customerMenuItemList = new ArrayList<MenuItem>();
+
+		for (int i = 0; i < menuItemList.size(); i++) {
+			MenuItem menuItem = menuItemList.get(i);
+			if ((menuItem.getDateOfLaunch().equals(new Date()) || menuItem.getDateOfLaunch().before(new Date()))
+					&& menuItem.isActive()) {
+				customerMenuItemList.add(menuItem);
+			}
+		}
+
+		return customerMenuItemList;
+	}
+
+	@Override
+	public void modifyMenuItem(MenuItem menuItem) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < menuItemList.size(); i++) {
+			if (menuItemList.get(i).equals(menuItem)) {
+				menuItemList.set(i, menuItem);
+				break;
+			}
+		}
+
+	}
+
+	@Override
+	public MenuItem getMenuItem(long menuItemId) {
+		// TODO Auto-generated method stub
+		MenuItem menuItem = null;
+		for (int i = 0; i < menuItemList.size(); i++) {
+			if (menuItemList.get(i).getId() == menuItemId) {
+				menuItem = menuItemList.get(i);
+				break;
+			}
+		}
+		return menuItem;
+	}
+
+}
